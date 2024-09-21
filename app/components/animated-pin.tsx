a655cdbbad046a4ca60a2e7273a34be1c490aae0
Enrico Pratama <enricopratama10@gmail.com>
2024-09-21 22:56:50 +0700

    updates changes

--- app/components/animated-pin.tsx
+++ app/components/animated-pin.tsx
@@ -79,11 +79,9 @@ export function AnimatedPinDemo() {
   ];
 
   return (
-    <div className="mt-16 md:mt-24 mx-auto">
+    <div className="mt-16 md:mt-24 mx-auto mb-16">
       <HeroHighlight>
         {/* Radial gradient for the container to give a faded look */}
-        {/* <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center"> */}
-        {/* <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div> */}
         <div className="h-[20rem]">
           <motion.h1
             initial={{
